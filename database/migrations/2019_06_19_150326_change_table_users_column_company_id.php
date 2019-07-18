da910ed15ce4bc5604e157b73f5c5c03a438967c
romario <romario_dindajal@hotmail.com>
2019-07-18 10:53:50 +0200

    drop foreign key contraint company id from user

--- database/migrations/2019_06_19_150326_change_table_users_column_company_id.php
+++ database/migrations/2019_06_19_150326_change_table_users_column_company_id.php
@@ -25,6 +25,8 @@ class ChangeTableUsersColumnCompanyId extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('companyId');
+        });
     }
 }
